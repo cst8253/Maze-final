7949b530d456bba28c3611be2a25a86811fbd478
Michael Eisenbraun <eisenbm@algonquincollege.com>
2024-05-26 21:46:42 -0400

    Creates starter files

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Each room is represented by a list
+
+Indexes
+0 - Room number
+1 - North Room
+2 - East Room
+3 - South Room
+4 - West Room
+
+If value is 0, there is no door in that direction.
+Reaching Room 10 will lead to escaping the maze. 
+*/
+List<List<int>> rooms = [
+  [1, 3, 2, 0, 0], // Room 1
+  [2, 0, 0, 0, 1], // Room 2
+  [3, 6, 0, 1, 0], // Room 3
+  [4, 0, 8, 2, 3], // Room 4
+  [5, 0, 7, 4, 6], // Room 5
+  [6, 0, 5, 3, 0], // Room 6
+  [7, 0, 0, 0, 0], // Room 7
+  [8, 7, 0, 9, 0], // Room 8
+  [9, 8, 0, 10, 2] // Room 9
+];
+List<string> indexes = ["ROOM", "NORTH", "EAST", "SOUTH", "WEST"];
+
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Maze
-This repository contains the starter files for the Maze project.
+
+## Objective
+Using the provided starter files, create a text-based maze. The player begins in Room 1 and will choose an available door to move to another room. 
